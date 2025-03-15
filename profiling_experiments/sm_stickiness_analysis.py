09e64d879022fdf39787b293fae94d0941e28244
Srivatsan Ramesh <srir@fb.com>
2025-03-14 20:05:49 -0700

    Op level analysis

--- profiling_experiments/sm_stickiness_analysis.py
+++ profiling_experiments/sm_stickiness_analysis.py
@@ -2,6 +2,8 @@ import argparse
 import json
 import os
 
+from collections import defaultdict
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--op", type=str, default="fp8_gemm")
 
@@ -10,16 +12,13 @@ args = parser.parse_args()
 script_dir = os.path.dirname(os.path.abspath(__file__))
 traces_dir = os.path.join(script_dir, args.op, "traces")
 
-data = {}
+data = defaultdict(lambda: defaultdict(int))
 
 for filename in os.listdir(traces_dir):
     with open(os.path.join(traces_dir, filename), "r") as f:
         d = json.load(f)
         for event in d["traceEvents"]:
-            if event["pid"] in data:
-                data[event["pid"]].add(event["args"]["sm_id"])
-            else:
-                data[event["pid"]] = {event["args"]["sm_id"]}
+            data[event["pid"]][event["args"]["sm_id"]] += 1
 
 with open(os.path.join(script_dir, args.op, "sm_stickiness.txt"), "w") as f:
     for threadblock, sm_ids in data.items():
